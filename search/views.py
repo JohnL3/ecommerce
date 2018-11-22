a551920493e2e6a74c7f477f0318436d7901462c
John L <johnlonggately@gmail.com>
2018-11-22 19:20:59 +0000

    sorting to push to heroku

--- search/views.py
+++ search/views.py
@@ -5,3 +5,4 @@ from products.models import Product
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, 'products.html', {'products': products})
+    
